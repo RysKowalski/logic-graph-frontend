09111174fe9b1ed2f07ce30f4b3e82bbd452d3c3
RysKowalski <ryskowalski0@gmail.com>
2026-01-14 14:12:15 +0100

    added JoinNode

--- src/nodes/JoinNode.tsx
+++ src/nodes/JoinNode.tsx
@@ -1,5 +1,11 @@
-import { NodeProps } from "@xyflow/react";
+import { Handle, NodeProps, Position } from "@xyflow/react";
 
 export default function JoinNode(props: NodeProps) {
-  return (<div />)
+  return (
+    <div>
+      <Handle type="target" position={Position.Top} />
+      <Handle type="source" position={Position.Bottom} />
+      <div className="join-node" />
+    </div>
+  )
 }
